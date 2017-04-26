4d9634ab249733d5568f94cdb440dad58544046f
derekarani <derekarani@gmail.com>
2017-04-26 12:38:06 +0300

    Framework complete and its working perfectly

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  $(".operators").click(function(){
+     alert("This means ------")
+  });
+  $(".variables").click(function(){
+     alert("This means ------")
+  });
+  $(".variable_naming").click(function(){
+     alert("This means ------")
+  });
+  $(".functions").click(function(){
+     alert("This means ------")
+  });
+  $(".strings").click(function(){
+     alert("This means ------")
+  });
+  $(".methods").click(function(){
+     alert("This means ------")
+  });
+  $(".arguments").click(function(){
+     alert("This means ------")
+  });
+  $(".parameters").click(function(){
+     alert("This means ------")
+  });
+  $(".return").click(function(){
+     alert("This means ------")
+  });
+  $(".chaining_methods").click(function(){
+     alert("This means ------")
+  });
+  $(".strings").click(function(){
+     alert("This means ------")
+  });
+  $(".booleans").click(function(){
+     alert("This means ------")
+  });
+  $(".undefined").click(function(){
+     alert("This means ------")
+  });
+  $(".nan").click(function(){
+     alert("This means ------")
+  });
+  $(".escape").click(function(){
+     alert("This means ------")
+  });
+  $(".alert").click(function(){
+     alert("This means ------")
+  });
+  $(".comments").click(function(){
+     alert("This means ------")
+  });
+  $(".jQuery").click(function(){
+     alert("This means ------")
+  });
+  $(".attributes").click(function(){
+     alert("This means ------")
+  });
+  $(".JS").click(function(){
+     alert("This means ------")
+  });
+
+
+
+});
